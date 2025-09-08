afa8312b83fd6b8ba0dff84f36c78fab7e357225
Fabio Spagliardi <spagliardi.fabio@gmail.com>
2025-09-08 17:38:18 +0100

    Add Search and Pagination

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -1,3 +1,50 @@
-export default function Page() {
-    return <p>Invoices Page</p>
+// How to use pagination
+//
+// useSearchParams- Allows you to access the parameters of the current URL. 
+// For example, the search params for this URL /dashboard/invoices?page=1&query=pending 
+// would look like this: {page: '1', query: 'pending'}.
+//
+// usePathname - Lets you read the current URL's pathname. For example, for the 
+// route /dashboard/invoices, usePathname would return '/dashboard/invoices'.
+//
+// useRouter - Enables navigation between routes within client components programmatically. 
+// There are multiple methods you can use.
+
+import Pagination from '@/app/ui/invoices/pagination';
+import Search from '@/app/ui/search';
+import Table from '@/app/ui/invoices/table';
+import { CreateInvoice } from '@/app/ui/invoices/buttons';
+import { lusitana } from '@/app/ui/fonts';
+import { InvoicesTableSkeleton } from '@/app/ui/skeletons';
+import { Suspense } from 'react';
+import { fetchInvoicesPages } from '@/app/lib/data';
+
+export default async function Page(props: {
+  searchParams?: Promise<{
+    query?: string;
+    page?: string;
+  }>;
+}) {
+  const searchParams = await props.searchParams;
+  const query = searchParams?.query || ''; console.log(query);
+  const currentPage = Number(searchParams?.page) || 1;
+  const totalPages = await fetchInvoicesPages(query); console.log("Total pages", totalPages);
+ 
+  return (
+    <div className="w-full">
+      <div className="flex w-full items-center justify-between">
+        <h1 className={`${lusitana.className} text-2xl`}>Invoices</h1>
+      </div>
+      <div className="mt-4 flex items-center justify-between gap-2 md:mt-8">
+        <Search placeholder="Search invoices..." />
+        <CreateInvoice />
+      </div>
+      <Suspense key={query + currentPage} fallback={<InvoicesTableSkeleton />}>
+        <Table query={query} currentPage={currentPage} />
+      </Suspense>
+      <div className="mt-5 flex w-full justify-center">
+        <Pagination totalPages={totalPages} />
+      </div>
+    </div>
+  );
 }
--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -1,8 +1,42 @@
 'use client';
 
+// Need to use debouncing for this component to make sure that each keystroke doesn't trigger a sql query
+// How Debouncing Works:
+// Trigger Event: When an event that should be debounced (like a keystroke in the search box) occurs, a timer starts.
+// Wait: If a new event occurs before the timer expires, the timer is reset.
+// Execution: If the timer reaches the end of its countdown, the debounced function is executed.
+// Install with 'pnpm i use-debounce'
+
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
+import { useSearchParams, usePathname, useRouter } from 'next/navigation';
+import { useDebouncedCallback } from 'use-debounce';
 
 export default function Search({ placeholder }: { placeholder: string }) {
+  const searchParams = useSearchParams();
+  const pathname = usePathname(); //console.log("Pathname:", pathname)
+  const { replace } = useRouter(); //console.log("Replace", replace)
+  
+  const handleSearch = useDebouncedCallback((term: string) => {
+    // URLSearchParams is a Web API that provides utility methods for manipulating the 
+    // URL query parameters. Instead of creating a complex string literal, you can use 
+    // it to get the params string like ?page=1&query=a.
+    //
+    // Here's a breakdown of what's happening:
+    // - ${pathname} is the current path, in your case, "/dashboard/invoices".
+    // - As the user types into the search bar, params.toString() translates this input into a URL-friendly format.
+    // - replace(${pathname}?${params.toString()}) updates the URL with the user's search data. For example, /dashboard/invoices?query=lee if the user searches for "Lee".
+    // - The URL is updated without reloading the page, thanks to Next.js's client-side navigation (which you learned about in the chapter on navigating between pages.
+
+    const params = new URLSearchParams(searchParams);
+    params.set('page', '1'); // Set 1 by default for new searches
+    if (term) {
+      params.set('query', term);
+    } else {
+      params.delete('query');
+    }
+    replace(`${pathname}?${params.toString()}`);
+  }, 300) // only execute if the use hasn't typed for 300ms
+
   return (
     <div className="relative flex flex-1 flex-shrink-0">
       <label htmlFor="search" className="sr-only">
@@ -11,8 +45,20 @@ export default function Search({ placeholder }: { placeholder: string }) {
       <input
         className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
         placeholder={placeholder}
+        onChange={e => {
+          handleSearch(e.target.value)
+        }}
+        defaultValue={searchParams.get('query')?.toString()} // To ensure the input field is in sync with the URL and will be populated when sharing
       />
       <MagnifyingGlassIcon className="absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
     </div>
   );
 }
+
+// defaultValue vs. value / Controlled vs. Uncontrolled
+
+// If you're using state to manage the value of an input, you'd use the value attribute to make it a
+// controlled component. This means React would manage the input's state.
+
+// However, since you're not using state, you can use defaultValue. This means the native input will 
+// manage its own state. This is okay since you're saving the search query to the URL instead of state.
