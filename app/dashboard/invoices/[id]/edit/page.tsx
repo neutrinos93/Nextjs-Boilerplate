b6de25ae298c87e3db73141518c25d5e88919579
Fabio Spagliardi <spagliardi.fabio@gmail.com>
2025-09-08 23:00:21 +0100

    Edit and delete invoices using Server Actions

---
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -0,0 +1,29 @@
+import Form from '@/app/ui/invoices/edit-form';
+import Breadcrumbs from '@/app/ui/invoices/breadcrumbs';
+import { fetchInvoiceById, fetchCustomers } from '@/app/lib/data';
+ 
+// Params is a special prop in NextJS router which is initialised and passed as the value of [id], [slug], etc
+export default async function Page(props: { params: Promise<{ id: string }> }) {
+  const params = await props.params;
+  const id = params.id;
+  const [invoice, customers] = await Promise.all([
+    fetchInvoiceById(id),
+    fetchCustomers(),
+  ]);
+  
+  return (
+    <main>
+      <Breadcrumbs
+        breadcrumbs={[
+          { label: 'Invoices', href: '/dashboard/invoices' },
+          {
+            label: 'Edit Invoice',
+            href: `/dashboard/invoices/${id}/edit`,
+            active: true,
+          },
+        ]}
+      />
+      <Form invoice={invoice} customers={customers} />
+    </main>
+  );
+}
--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -1,6 +1,7 @@
 'use server';
 // This marks all the exported functions as Server Actions
 // These functions can be imported and used in Client and Server components.
+// Read more about security with Server Actions at https://nextjs.org/blog/security-nextjs-server-components-actions
 
 import { z } from 'zod'; // To type-validation the form
 import { revalidatePath } from 'next/cache'; // Used to clear cached router segments in browser since we are updating displayed data
@@ -18,6 +19,7 @@ const FormSchema = z.object({
 })
 
 const CreateInvoice = FormSchema.omit({id: true, date: true});
+const UpdateInvoice = FormSchema.omit({id: true, date: true});
 
 export async function createInvoice(formData: FormData) {
   // Tip: If you're working with forms that have many fields, you may want 
@@ -37,4 +39,27 @@ export async function createInvoice(formData: FormData) {
 
   revalidatePath('/dashboard/invoinces');
   redirect('/dashboard/invoices');
+}
+
+export async function updateInvoice(id: string, formData: FormData) {
+  const {customerId, amount, status} = UpdateInvoice.parse({
+    customerId: formData.get('customerId'),
+    amount: formData.get('amount'),
+    status: formData.get('status'),
+  });
+
+  const amountInCents = amount * 100;
+
+  await sql`
+    UPDATE invoices
+    SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
+    WHERE id = ${id}
+  `;
+  revalidatePath('/dashboard/invoices');
+  redirect('/dashboard/invoices');
+}
+
+export async function deleteInvoice(id: string) {
+  await sql`DELETE FROM invoices WHERE id = ${id}`;
+  revalidatePath('/dashboard/invoices');
 }
