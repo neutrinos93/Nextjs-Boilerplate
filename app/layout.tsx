abc240f0994419cd7a8d3c7b156c60dfa581c3a1
Fabio Spagliardi <spagliardi.fabio@gmail.com>
2025-09-10 00:11:32 +0100

    Add some metadata using the NextJS Metadata API

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,16 @@
 import '@/app/ui/global.css'
 import { inter } from '@/app/ui/fonts'
+import { Metadata } from 'next';
+
+// This is automatically inherited by all pages that use this layout.
+export const metadata: Metadata = {
+  title: {
+    template: '%s | Acme Dashboard', // The %s will be replaced with the specific page title (See /dashboard/invoices/page.tsx)
+    default: 'Acme Dashboard',
+  },
+  description: 'The official Next.js Course Dasbhoard, built with App Router.',
+  metadataBase: new URL(''),
+}
 
 export default function RootLayout({
   children,
